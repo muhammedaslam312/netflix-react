1f1607b746551d3e8a5e0d278218f6e879d42b99
Muhammed Aslam <muhammmedaslamm312@gmail.com>
2022-09-26 11:07:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {action,originals} from './urls'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = "383edc00cb74e96dc8c79fccb3c227f2";
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
